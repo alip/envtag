bfea58ff295b2622cd9e75ab2c0460d41642cc58
Ali Polatel <polatel@gmail.com>
2009-04-06 20:47:25 +0300

    Update capitalize.lua

--- examples/lua/capitalize.lua
+++ examples/lua/capitalize.lua
@@ -7,13 +7,39 @@
 local gsub = string.gsub
 local upper = string.upper
 local sub = string.sub
+local insert = table.insert
 
 function capitalize(str)
     return gsub(str, "(%w+)", function (c) return upper(sub(c, 1, 1)) .. sub(c, 2) end)
 end
 
-artist = assert(tag.get"artist")
-cartist = capitalize(artist)
-if VERBOSE then print(artist, "-->", cartist) end
-tag.set("artist", cartist)
-file.save()
+if tag.has_xiph() then -- Xiph Comment interface
+    artist = tag.get_xiph("artist", opt.unicode)
+else -- Common Tag interface
+    artist = tag.get("artist", opt.unicode)
+end
+
+if artist then -- Artist set
+    if tag.has_xiph() then -- Xiph Comment
+        local at = {}
+        for _, name in ipairs(artist) do
+            local cname = capitalize(name)
+            if opt.verbose then
+                print(name, "-->", cname)
+            end
+            insert(at, cname)
+        end
+        -- Clear ARTIST field
+        tag.set_xiph("artist", false, {""})
+        -- Set new ARTIST field
+        tag.set_xiph("artist", true, at)
+    else -- Common Tag
+        local cartist = capitalize(artist)
+        if opt.verbose then
+            print(artist, "-->", cartist)
+        end
+        tag.set("artist", cartist)
+    end
+    file.save()
+end
+
