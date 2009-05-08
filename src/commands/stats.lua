alip/envtag

commit 4d9e2084be3398244a5e485a72876445989599aa

stats: make envutils.pretty_time a local function of stats command

src/commands/stats.lua
@@ -27,6 +27,20 @@ local function top(list, count)
     return best
 end
 
+local SECSPERDAY = 86400
+local SECSPERHOUR = 3600
+local SECSPERMIN = 60
+local function pretty_time(t)
+    days = t / SECSPERDAY
+    t = t % SECSPERDAY
+    hours = t / SECSPERHOUR
+    t = t % SECSPERHOUR
+    mins = t / SECSPERMIN
+    t = t % SECSPERMIN
+    secs = t
+    return string.format("%d days, %d:%02d:%02d", days, hours, mins, secs)
+end
+
 local function usage()
     print"envtag stats -- Show statistics"
     print"Usage: envtag stats [-hvn] [-t TYPE] file..."

src/modules/envutils.lua
@@ -50,20 +50,6 @@ function split(src, char)
     return list
 end
 
-local SECSPERDAY = 86400
-local SECSPERHOUR = 3600
-local SECSPERMIN = 60
-function pretty_time(t)
-    days = t / SECSPERDAY
-    t = t % SECSPERDAY
-    hours = t / SECSPERHOUR
-    t = t % SECSPERHOUR
-    mins = t / SECSPERMIN
-    t = t % SECSPERMIN
-    secs = t
-    return string.format("%d days, %d:%02d:%02d", days, hours, mins, secs)
-end
-
 TAGS_COMMON = {
     "title", "artist", "album", "comment",
     "genre", "year", "track"

commit e8801a9bc66f56fea3304d3ee12332749bf48ea6

stats: fix -p broken by 4d9e208

src/commands/stats.lua
@@ -171,7 +171,7 @@ print("Artists: " .. nartist)
 print("Genres: " .. ngenre)
 print("Songs: " .. nsong)
 if readprops then
-    print("Total play time: " .. envutils.pretty_time(length))
+    print("Total play time: " .. pretty_time(length))
 end
 
 top_artists = top(artist_list, topcount)