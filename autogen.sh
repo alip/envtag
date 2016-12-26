bc56b0b0342e85022f00ee26c959cc27c4ec4336
Ali Polatel <alip@exherbo.org>
2016-12-26 11:06:39 +0100

    update autotools

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,9 @@
-#!/bin/sh
+#!/bin/sh -ex
 # vim: set sw=4 et sts=4 tw=80 :
 
-die() {
-    echo "$@" >&2
-    exit 1
-}
-
-echo ">>> rm -f config.cache"
-rm -f config.cache
-echo ">>> aclocal"
-aclocal || die "aclocal failed"
-echo ">>> autoheader"
-autoheader || die "autoheader failed"
-echo ">>> autoconf"
-autoconf || die "autoconf failed"
-echo ">>> automake --foreign --add-missing --copy"
-automake --foreign --add-missing --copy || die "automake failed"
+rm -fr autom4te.cache build-aux config.cache
+mkdir -p m4 build-aux
+aclocal
+autoheader
+autoconf
+automake --add-missing --copy
